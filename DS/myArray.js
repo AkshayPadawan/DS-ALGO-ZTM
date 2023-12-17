07a0ca2e0683e5ffb836a3d6c88fccca6c2ff50f
AkshayPadawan <45865564+AkshayPadawan@users.noreply.github.com>
2023-12-17 17:25:39 +0000

    Array

--- DS/myArray.js
+++ DS/myArray.js
@@ -1,6 +1,43 @@
 class MyArray {
   constructor() {
+    this.length = 0;
     this.data = {};
   }
+  get(index) {
+    return this.data[index];
+  }
+  push(value) {
+    this.data[this.length++] = value;
+    return this.length;
+  }
+  delete(index) {
+    const value = this.data[index];
+    for (let i = index; i < this.length - 1; i++) {
+      this.data[i] = this.data[i + 1];
+    }
+    delete this.data[this.length-1];
+    this.length--;
+    return value;
+  }
+  pop() {
+    const value = this.data[this.length - 1];
+    delete this.data[this.length - 1];
+    this.length--;
+    return value;
+  }
 }
-console.log("scscsc");
+const array = new MyArray();
+console.log(array);
+console.log(array.get(1));
+array.push("a");
+console.log(array.get(0));
+array.push("b");
+array.push("c");
+console.log(array);
+console.log(array.pop());
+array.push("d");
+console.log(array);
+array.push("e");
+console.log(array);
+console.log(array.delete(2));
+console.log(array);
