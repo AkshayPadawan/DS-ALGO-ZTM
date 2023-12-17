a51d0ffa0a53c81b23406130b695f86f7777b77b
AkshayPadawan <45865564+AkshayPadawan@users.noreply.github.com>
2023-12-17 16:53:59 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const strings = ["a","b","c","d",];
+
+strings.push("f");
+console.log(strings)
+strings.pop("f");
+console.log(strings);
+strings.unshift("f");
+console.log(strings)
+strings.splice(3,0,"f");
+console.log(strings);
